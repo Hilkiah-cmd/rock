a58be2a2160c2c26b9c25462da1503c4a96d7ace
Hilkiah-cmd <Hil02may@gmail.com>
2022-03-28 15:46:13 +0300

    Been a whole lot of time but finally my syntax works well. Done with the game() function

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
+let compScore = 0;
+let playerScore = 0;
+let result;
 function computerplay(){
     var compoptions = ["rock","paper","scissors"];
-    let compchoice = compoptions[Math.floor(Math.random() * compoptions.length)]
-    console.log("Computer choose = " + compchoice)
+    let compchoice = compoptions[Math.floor(Math.random() * compoptions.length)];
+    console.log("Computer choose = " + compchoice);
     return compchoice;
 }
-function playRound(playerSelection,compSelection){
-    // playerSelection = playerSelection.toLowerCase();
-    // compSelection = compSelection.toLowerCase();
+function playRound(playerSelection){
+    playerSelection = window.prompt("Enter you choice: rock,paper,scissor");
+    playerSelection = playerSelection.toLowerCase();
+    let compSelection = computerplay();
+    console.log("You choose =" +playerSelection)
     if(playerSelection === compSelection){
-        return("You tie!")
+        console.log("You tie!")
     }
     else if(compSelection === 'scissors'){
-        return("You win!Rock beats Scissors")
+    switch(playerSelection) {
+        case 'paper': 
+            console.log("You lose!Scissor beats paper");
+            return '0';
+            
+        case 'rock':
+            console.log("You win!Rock beats scissors");
+            return '1';
+            
+    }}
+    else if(compSelection === 'paper'){
+        switch(playerSelection){
+            case 'rock': 
+            console.log("You lose!Paper beats rock");
+            return '0';
+            
+        case 'scissors':
+            console.log("You win!Scissors beats paper");
+            return '1';
+            
+        }
     }
-    else {
-        return("You lose!Paper beats Rock")
+    else{
+        console.log("Invalid entry");
     }
 }
-const playerSelection = "rock";
-console.log("Player choose = "+ playerSelection);
-const compSelection = computerplay();
-playRound();
-console.log(playRound(playerSelection, compSelection));
+function game(){
+    for(let i=0;i<5;i++){
+         result = playRound();
+         if(result === '1'){
+            playerScore = playerScore + 1;
+        }else if(result === '0'){
+            compScore = compScore +1;
+        }
+    }
+    console.log("Your score = "+playerScore+" wins");
+    console.log("Computer score = "+compScore+" wins")
+    if(playerScore === compScore){
+        console.log("It's a draw");
+    }else if(playerScore > compScore){
+        console.log("You win!Bravo");
+    }else if(compScore > playerScore){
+        console.log("You lose!Try Again Next time")
+    }
+}
+game();
+
+// const playerSelection = "rock";
+// console.log("Player choose = "+ playerSelection);
+// const compSelection = computerplay();
+// playRound();
+// console.log(playRound(playerSelection, compSelection));
